7bb900d55a6cf40f5b8eb4fa1aa86fdfd3c78f4d
Kavan <kavan.soni25@gmail.com>
2022-12-28 01:29:37 -0800

    Update 69. Sqrt(x).py

--- 69. Sqrt(x).py
+++ 69. Sqrt(x).py
@@ -1,10 +1,22 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-
+        '''
         if x <=1: return x
-        
         answer = 1
         for i in range(1, x//2 + 1):
             if i*i <= x: answer = i
             else: break
         return answer
+        '''
+        if x <=1 : return x
+        l, r = 1, x
+
+        while l<r:
+
+            m = l + (r-l)//2
+            if m * m <= x : l = m+1
+            else: r = m
+        
+        return l-1
+
+
