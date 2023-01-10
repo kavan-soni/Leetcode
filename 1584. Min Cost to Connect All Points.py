6edde03f1cb82502f15adb95fff95bcd7db5c65e
Kavan <kavan.soni25@gmail.com>
2023-01-10 14:59:46 -0800

    Update 1584. Min Cost to Connect All Points.py

--- 1584. Min Cost to Connect All Points.py
+++ 1584. Min Cost to Connect All Points.py
@@ -1,8 +1,43 @@
 class Solution:
     def minCostConnectPoints(self, p: List[List[int]]) -> int:
-        
+
+        # Prim's Algorithm
+
         n = len(p)
-        edges = list([i,j,abs(p[j][0] - p[i][0]) + abs(p[j][1] - p[i][1])] for i in range(n) for j in range(i+1 ,n)) 
+        adj = collections.defaultdict(list)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = abs(p[i][1]-p[j][1]) + abs(p[i][0]-p[j][0])
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        q = [[0,0]] #[dist, start_idx]
+        visited = set()
+
+        total_cost = 0
+
+        while q and len(visited) < n:
+            dist_to_node, node_idx = heapq.heappop(q)
+            if node_idx in visited: continue
+            
+            total_cost += dist_to_node
+            visited.add(node_idx)
+            for dist_to_neighbor, neighbor_idx in adj[node_idx]:
+                if neighbor_idx in visited: continue
+                heapq.heappush(q, [dist_to_neighbor, neighbor_idx])
+
+        return total_cost
+            
+
+        '''
+        # Kruskal's Algorithm
+        n = len(p)
+
+        def edge(i,j):
+            return [i,j,abs(p[j][0] - p[i][0]) + abs(p[j][1] - p[i][1])]
+            
+        edges = list(edge(i,j) for i in range(n) for j in range(i+1 ,n)) 
         edges = sorted(edges, key = lambda x : x[2])
 
         parent = [i for i in range(n)]
@@ -25,3 +60,16 @@ class Solution:
             cost += dist
             
         return cost 
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
