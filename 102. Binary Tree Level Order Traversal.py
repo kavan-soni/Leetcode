a5124ac6b3bc71877a3ca062afa7a36a4803db48
Kavan <kavan.soni25@gmail.com>
2023-02-05 15:20:01 -0800

    Update 102. Binary Tree Level Order Traversal.py

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -12,7 +12,7 @@ class Solution:
 
         def helper(node, level):
             if not node: return
-            if level not in levels: ans.append([node.val]); levels.add(level)
+            if level not in levels: ans.o([node.val]); levels.add(level)
             else: ans[level].append(node.val)
             helper(node.left, level+1)
             helper(node.right, level+1)
