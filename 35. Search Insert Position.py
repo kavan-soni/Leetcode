83209b8e2ec17007392efc7d5f41713940d53704
Kavan <kavan.soni25@gmail.com>
2023-02-05 16:53:11 -0800

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,10 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
-        l, r = 0, len(nums)
+        l, r = 0, len(nums) # since target can be greater than max value in array, right boundary is set as len(nums)
+
+        def condition(x):
+            return nums[x]>=target
 
         while l<r:
             m = l + (r-l)//2
-            if target <= nums[m]: r = m
+            if condition(m): r = m
             else: l = m +1
-        return l 
+        return l
